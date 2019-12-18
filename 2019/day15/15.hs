00c67efc26b48b7af7db099174fbe9ac391280c7
John Andersson <johnand@student.chalmers.se>
2019-12-18 08:11:45 +0100

    Rewrite intcode computer to use state monad

---
+++ 2019/day15/15.hs
@@ -0,0 +1,180 @@
+
+import Control.Monad
+import Data.List.Split
+import Data.List hiding ((!!), insert)
+import Prelude hiding ((!!))
+import Data.IntMap (IntMap, insert)
+import qualified Data.IntMap as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+
+import Control.Monad.State
+
+main :: IO ()
+main = a
+
+a :: IO ()
+a = do
+  input <- readInput "input.txt"
+  mainA input
+
+b :: IO ()
+b = do
+  input <- readInput "input.txt"
+  mainB input
+
+test1 :: IO ()
+test1 = do
+  input <- readInput "test1.txt"
+  mainA input
+
+----------- Code for part b ------------------
+mainB :: IntMap Integer -> IO ()
+mainB ls = do
+  putStrLn "Not yet implemented"
+----------- Code for part b ------------------
+----------- Code for part a ------------------
+
+mainA :: IntMap Integer -> IO ()
+mainA ls = do
+  let env = emptyEnv ls
+  let state = execState performProgram env
+  print $ output state
+  return ()
+
+type Program = (Integer, Integer,GameBoard,Int,(Int, IntMap Integer))
+
+(!) :: IntMap Integer -> Int -> Integer
+(!) mapp key = IntMap.findWithDefault 0 key mapp
+
+type GameBoard = Map Position Integer
+type Position = (Int,Int)
+
+emptyEnv :: IntMap Integer -> Env
+emptyEnv prg = Env {
+  rbase = 0,
+  index = 0,
+  output = [],
+  program = prg
+}
+
+data Env = Env {
+  rbase :: Integer,
+  index :: Int,
+  output :: [Integer],
+  program :: IntMap Integer
+}
+
+setIndex :: Integer -> State Env ()
+setIndex i = modify $ \s -> s{index = (frI i)}
+
+updateRbase :: Integer -> State Env ()
+updateRbase i = do
+  r <- gets rbase
+  modify $ \s -> s{rbase = r + i}
+
+incrementIndex :: Int -> State Env ()
+incrementIndex i = do
+  index' <- gets index
+  modify $ \s -> s{index = index' + i}
+
+updatePrg :: Int -> Integer -> State Env ()
+updatePrg ind val = do
+  prg <- gets program
+  modify $ \s -> s{program = IntMap.insert ind val prg}
+
+saveOutput :: Integer -> State Env ()
+saveOutput int = do
+  outpt <- gets output
+  modify $ \s -> s{output = int:outpt}
+
+input :: Integer
+input = 2
+
+performProgram :: State Env ()
+performProgram = do
+    ind <- gets index
+    prg <- gets program
+    rbas <- gets rbase
+    let (modes, op) = splitAt 3 $ genNum $ prg ! ind
+    let [v3,v2,v1] = getValues (ind + 1) prg modes rbas
+    case op of
+      [_,1] -> do
+        updatePrg (frI v3) (v1 + v2)
+        incrementIndex 4
+        performProgram
+      [_,2] -> do
+        updatePrg (frI v3) (v1 * v2)
+        incrementIndex 4
+        performProgram
+      [_,3] -> do
+        let index1 = calculateIndex (ind + 1) prg (last modes) rbas
+        updatePrg (frI index1) input
+        incrementIndex 2
+        performProgram
+      [_,4] -> do
+        saveOutput v1
+        incrementIndex 2
+        performProgram
+      [_,5] -> do
+        if v1 /= 0 then setIndex v2
+        else incrementIndex 3
+        performProgram
+      [_,6] -> do
+        if v1 == 0 then setIndex v2
+        else incrementIndex 3
+        performProgram
+      [_,7] -> do
+        if v1 < v2 then updatePrg (frI v3) 1
+        else updatePrg (frI v3) 0
+        incrementIndex 4
+        performProgram
+      [_,8] -> do
+        if v1 == v2 then updatePrg (frI v3) 1
+        else updatePrg (frI v3) 0
+        incrementIndex 4
+        performProgram
+      [0,9] -> do
+        updateRbase v1
+        incrementIndex 2
+        performProgram
+      [9,9] -> do
+        return ()
+
+frI = fromInteger
+
+getValues :: Int -> IntMap Integer -> [Integer] -> Integer-> [Integer]
+getValues index ls [m3,m2,m1] rbase = [index3, ls ! (frI index2), ls ! (frI index1)]
+  where
+    index1 = calculateIndex index ls m1 rbase
+    index2 = calculateIndex (index + 1) ls m2 rbase
+    index3 = calculateIndex (index + 2) ls m3 rbase
+
+calculateIndex :: Int -> IntMap Integer -> Integer -> Integer -> Integer
+calculateIndex index ls mode rbase = case mode of
+  2 -> rbase + (ls ! index)
+  1 -> toInteger index
+  0 -> ls ! index
+
+genNum :: Integer -> [Integer]
+genNum x = [mod (x `div` 10000) 10,
+            mod (x `div` 1000) 10,
+            mod (x `div` 100) 10,
+            mod (x `div` 10) 10,
+            mod x 10]
+----------- Code for part a ------------------
+
+
+----------- Input ----------------------------
+readInput :: String -> IO (IntMap Integer)
+readInput filePath = do
+  input <- readFile filePath
+  return $ formatInput input
+
+formatInput :: String -> IntMap Integer
+formatInput input = intMap
+  where
+    intMap = IntMap.fromList ls'
+    ls = map read $ splitOn "," (head (lines input))
+    ls' = zip [0..] ls
